afe1facd4e8b077576140ac0de6ac9407d769eed
Ramsey Chapin <ramsey@ramseychapin.com>
2019-07-04 00:54:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -46,7 +46,7 @@ class Owner
   end
 
   def sell_pets
-    self.cats.map { |cat| cat.mood = "happy" }
+    self.cats.map { |cat| cat.mood = "nervous" }
     self.dogs.map { |dog| dog.mood = "happy" }
     self.cats.clear
     self.dogs.clear
